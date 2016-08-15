f8c2625df491cd475e04bb8068de61c8c8b62d71
aleksandra shineleva <deadcookies@gmail.com>
2016-08-14 21:32:05 -0400

    website created

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(function(){
+	$('#menu').slicknav();
+});
+
+	    $(document).ready(function(){
+	    	var parameters = {
+				dots: false,
+				autoplay: true,
+				autoPlayTimer: 3000,
+				infinite: true,
+				speed: 500,
+				cssEase: 'linear',
+				variableWidth: true,
+				swipe: true,
+				mobileFirst: true,
+				lazyLoad: 'ondemand',
+			}
+
+	      	$('.fade').slick(parameters);
+	      	
+	    });
+	    function showMenu() {
+	      		document.getElementById("mobile").classList.toggle("hidden");
+	      	}
+	    $("body").click(function(event){
+	    	var menu = $("#mobile");
+	    	if (event.target.id !=="menubtn" && !$('#mobile').has(event.target).length){
+	    		if (!menu.hasClass("hidden")){
+	    			menu.addClass("hidden")
+	    		}
+	    	}
+	    })
